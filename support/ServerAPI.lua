f9b4cbdf869e10366f6107909aa89b58020a72a4
Robert Chiquini <gigsd4x@gmail.com>
2016-05-09 00:24:03 -0700

    Add new material tool
    Signed-off-by: Robert Chiquini <gigsd4x@gmail.com>

--- support/ServerAPI.lua
+++ support/ServerAPI.lua
@@ -1154,6 +1154,48 @@ Actions = {
 
 	end;
 
+	['SyncMaterial'] = function (Changes)
+		-- Updates parts server-side given their new material
+
+		-- Grab a list of every part we're attempting to modify
+		local Parts = {};
+		for _, Change in pairs(Changes) do
+			if Change.Part then
+				table.insert(Parts, Change.Part);
+			end;
+		end;
+
+		-- Cache up permissions for all private areas
+		local AreaPermissions = Security.GetPermissions(Security.GetSelectionAreas(Parts), Player);
+
+		-- Make sure the player is allowed to perform changes to these parts
+		if Security.ArePartsViolatingAreas(Parts, Player, AreaPermissions) then
+			return;
+		end;
+
+		-- Reorganize the changes
+		local ChangeSet = {};
+		for _, Change in pairs(Changes) do
+			if Change.Part then
+				ChangeSet[Change.Part] = Change;
+			end;
+		end;
+
+		-- Perform each change
+		for Part, Change in pairs(ChangeSet) do
+			if Change.Material ~= nil then
+				Part.Material = Change.Material;
+			end;
+			if Change.Transparency ~= nil then
+				Part.Transparency = Change.Transparency;
+			end;
+			if Change.Reflectance ~= nil then
+				Part.Reflectance = Change.Reflectance;
+			end;
+		end;
+
+	end;
+
 };
 
 -- Provide functionality to the API instance
