c6cfb422bdf83eec07111b10ac780a5ab3444872
Robert Chiquini <gigsd4x@gmail.com>
2016-05-08 23:41:05 -0700

    Add new anchor tool
    Signed-off-by: Robert Chiquini <gigsd4x@gmail.com>

--- support/ServerAPI.lua
+++ support/ServerAPI.lua
@@ -1086,6 +1086,40 @@ Actions = {
 
 	end;
 
+	['SyncAnchor'] = function (Changes)
+		-- Updates parts server-side given their new anchor status
+
+		-- Grab a list of every part we're attempting to modify
+		local Parts = {};
+		for _, Change in pairs(Changes) do
+			if Change.Part then
+				table.insert(Parts, Change.Part);
+			end;
+		end;
+
+		-- Cache up permissions for all private areas
+		local AreaPermissions = Security.GetPermissions(Security.GetSelectionAreas(Parts), Player);
+
+		-- Make sure the player is allowed to perform changes to these parts
+		if Security.ArePartsViolatingAreas(Parts, Player, AreaPermissions) then
+			return;
+		end;
+
+		-- Reorganize the changes
+		local ChangeSet = {};
+		for _, Change in pairs(Changes) do
+			if Change.Part then
+				ChangeSet[Change.Part] = Change;
+			end;
+		end;
+
+		-- Perform each change
+		for Part, Change in pairs(ChangeSet) do
+			Part.Anchored = Change.Anchored;
+		end;
+
+	end;
+
 };
 
 -- Provide functionality to the API instance
