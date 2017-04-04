7c1f786d0a71e21e1eeff92b2cce07f7cf570e37
Robert Chiquini <gigsd4x@gmail.com>
2017-04-04 02:50:44 -0700

    Fix prism selection with manual intersection checking
    A bug in the Region3 module from AxisAngle seemed to have
    been returning inaccurate part casts, so this change replaces
    the use of that library with ROBLOX's GetTouchingParts API
    (temporarily enabling collision in potential parts to work
    around collision requirement).
    
    Signed-off-by: Robert Chiquini <gigsd4x@gmail.com>

--- Core.lua
+++ Core.lua
@@ -412,19 +412,47 @@ AssignHotkey({ 'RightShift', 'X' }, DeleteSelection);
 function PrismSelect()
 	-- Selects parts in the currently selected parts
 
+	-- Ensure parts are selected
+	if #Selection.Items == 0 then
+		return;
+	end;
+
+	-- Get region for selection items and find potential parts
+	local Extents = require(Tool.BoundingBoxModule).CalculateExtents(Selection.Items, nil, true);
+	local Region = Region3.new(Extents.Min, Extents.Max);
+	local PotentialParts = Workspace:FindPartsInRegion3WithIgnoreList(Region, Selection.Items, math.huge);
+
+	-- Enable collision on all potential parts
+	local OriginalState = {};
+	for _, PotentialPart in pairs(PotentialParts) do
+		OriginalState[PotentialPart] = { Anchored = PotentialPart.Anchored, CanCollide = PotentialPart.CanCollide };
+		PotentialPart.Anchored = true;
+		PotentialPart.CanCollide = true;
+	end;
+
 	local Parts = {};
 
-	-- Go through each selected part
+	-- Find all parts intersecting with selection
 	for _, Part in pairs(Selection.Items) do
-		local Region = Region.FromPart(Part);
-		Support.ConcatTable(Parts, Region:Cast());
+		local TouchingParts = Part:GetTouchingParts();
+		for _, TouchingPart in pairs(TouchingParts) do
+			if not Selection.Find(TouchingPart) then
+				Parts[TouchingPart] = true;
+			end;
+		end;
+	end;
+
+	-- Restore all potential parts' original states
+	for PotentialPart, State in pairs(OriginalState) do
+		PotentialPart.CanCollide = State.CanCollide;
+		PotentialPart.Anchored = State.Anchored;
 	end;
 
 	-- Delete the selection parts
 	DeleteSelection();
 
 	-- Select all found parts
-	Selection.Replace(Parts, true);
+	Selection.Replace(Support.Keys(Parts), true);
 
 end;
 
