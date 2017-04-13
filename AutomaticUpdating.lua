c75a65131e45f646ed8d8aadec0ae16e03bbbb23
Robert Chiquini <gigsd4x@gmail.com>
2017-04-13 06:56:57 -0700

    Fix automatic updating by using a module to load the new tool
    This change switches the loading method for autoupdating the tool
    to requiring a module containing the latest tool asset. It should
    resolve issues with InsertService not being able to load the tool
    in group places due to not owning the asset, since modules can be
    required from any place regardless of whether they are owned by
    the place owner or not.
    
    Signed-off-by: Robert Chiquini <gigsd4x@gmail.com>

--- AutomaticUpdating.lua
+++ AutomaticUpdating.lua
@@ -1,12 +1,12 @@
 local Tool = script.Parent;
 
-function IsVersionOutdated()
-	-- Returns whether this version of Building Tools is out of date
+function IsVersionOutdated(Version)
+	-- Returns whether the given version of Building Tools is out of date
 
 	-- Check most recent version number
 	local AssetInfo = Game:GetService('MarketplaceService'):GetProductInfo(142785488, Enum.InfoType.Asset);
 	local LatestMajorVersion, LatestMinorVersion, LatestPatchVersion = AssetInfo.Description:match '%[Version: ([0-9]+)%.([0-9]+)%.([0-9]+)%]';
-	local CurrentMajorVersion, CurrentMinorVersion, CurrentPatchVersion = Tool.Version.Value:match '([0-9]+)%.([0-9]+)%.([0-9]+)';
+	local CurrentMajorVersion, CurrentMinorVersion, CurrentPatchVersion = Version:match '([0-9]+)%.([0-9]+)%.([0-9]+)';
 
 	-- Convert version data into numbers
 	local LatestMajorVersion, LatestMinorVersion, LatestPatchVersion =
@@ -31,17 +31,27 @@ function IsVersionOutdated()
 end;
 
 -- Ensure tool mode is enabled, auto-updating is enabled, and version is outdated
-if not (Tool:IsA 'Tool' and Tool.AutoUpdate.Value and IsVersionOutdated()) then
+if not (Tool:IsA 'Tool' and Tool.AutoUpdate.Value and IsVersionOutdated(Tool.Version.Value)) then
 	return;
 end;
 
--- Attempt to get latest copy of the asset
-local LatestVersionId = Game:GetService('InsertService'):GetLatestAssetVersionAsync(142785488);
-local Model = Game:GetService('InsertService'):LoadAssetVersion(LatestVersionId);
-if Model and #Model:GetChildren() > 0 then
+-- Use module to insert latest tool
+local GetLatestTool = require(580330877);
+if not GetLatestTool then
+	return;
+end;
 
-	-- Get the tool
-	local NewTool = Model:GetChildren()[1];
+-- Get latest copy of tool
+local NewTool = GetLatestTool();
+if NewTool then
+
+	-- Prevent update attempt loops since fetched version is now cached
+	NewTool.AutoUpdate.Value = false;
+
+	-- Cancel replacing current tool if fetched version is the same
+	if NewTool.Version.Value == Tool.Version.Value then
+		return;
+	end;
 
 	-- Detach update script from tool and save old tool parent
 	script.Parent = nil;
