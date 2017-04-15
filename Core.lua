a13bf2b221e6f76ece7a549c9b026d811828f7c9
Robert Chiquini <gigsd4x@gmail.com>
2017-04-15 05:37:29 -0700

    Disable tool if parent becomes invalid
    This change properly disables the tool when it is parented away
    from the player, thereby removing issues with uncleared connections
    when the tool is dropped or deleted.
    
    Signed-off-by: Robert Chiquini <gigsd4x@gmail.com>

--- Core.lua
+++ Core.lua
@@ -209,7 +209,9 @@ function Disable()
 	end;
 
 	-- Hide UI
-	UI.Parent = script;
+	if UI then
+		UI.Parent = script;
+	end;
 
 	-- Unequip current tool
 	if CurrentTool then
@@ -220,6 +222,18 @@ function Disable()
 	-- Clear temporary connections
 	ClearConnections();
 
+end;
+
+
+-- Core connections
+Connections = {};
+
+function ClearConnections()
+	-- Clears and disconnects temporary connections
+	for Index, Connection in pairs(Connections) do
+		Connection:disconnect();
+		Connections[Index] = nil;
+	end;
 end;
 
 function InitializeUI()
@@ -294,17 +308,18 @@ elseif Mode == 'Tool' then
 	Tool.Equipped:connect(Enable);
 	Tool.Unequipped:connect(Disable);
 
-end;
-
--- Core connections
-Connections = {};
-
-function ClearConnections()
-	-- Clears and disconnects temporary connections
-	for Index, Connection in pairs(Connections) do
-		Connection:disconnect();
-		Connections[Index] = nil;
+	-- Disable the tool if not parented
+	if not Tool.Parent then
+		Disable();
 	end;
+
+	-- Disable the tool automatically if not equipped or in backpack
+	Tool.AncestryChanged:connect(function (Item, Parent)
+		if not Parent or not (Parent:IsA 'Backpack' or (Parent:IsA 'Model' and Players:GetPlayerFromCharacter(Parent))) then
+			Disable();
+		end;
+	end);
+
 end;
 
 -- Assign hotkeys for undoing (left or right shift + Z)
