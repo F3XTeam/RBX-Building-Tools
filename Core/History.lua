d08daa1c5059b62927faa5fac28d824b68a180df
Jim Jimminson <bombaltcrismon@gmail.com>
2025-07-09 21:10:41 -0700

    Added Windows81's edge-case bugfix for the History module

--- Core/History.lua
+++ Core/History.lua
@@ -13,9 +13,18 @@ History = {
 	Index = 0,
 
 	-- History change event
-	Changed = Signal.new()
+	Changed = Signal.new(),
+	
+	-- To prevent edge cases where multiple actions take place at the same time 
+	Debounce = false
 
 };
+
+function WaitForDebounce()
+	while History.Debounce do
+		task.wait();
+	end;
+end;
 
 function History.Undo()
 	-- Unapplies the previous record in stack
@@ -24,7 +33,11 @@ function History.Undo()
 	if History.Index - 1 < 0 then
 		return;
 	end;
-
+	
+	-- Prevent edit conflicts
+	WaitForDebounce();
+	History.Debounce = true;
+	
 	-- Get the history record, unapply it
 	local Record = History.Stack[History.Index];
 	Record:Unapply();
@@ -34,6 +47,9 @@ function History.Undo()
 
 	-- Fire the Changed event
 	History.Changed:Fire();
+	
+	-- Release debounce lock
+	History.Debounce = false;
 
 end;
 
@@ -44,6 +60,10 @@ function History.Redo()
 	if History.Index + 1 > #History.Stack then
 		return;
 	end;
+	
+	-- Prevent edit conflicts
+	WaitForDebounce();
+	History.Debounce = true;
 
 	-- Update the index
 	History.Index = History.Index + 1;
@@ -54,11 +74,18 @@ function History.Redo()
 
 	-- Fire the Changed event
 	History.Changed:Fire();
+	
+	-- Release debounce lock
+	History.Debounce = false;
 
 end;
 
 function History.Add(Record)
 	-- Adds new history record to stack
+	
+	-- Prevent edit conflicts
+	WaitForDebounce();
+	History.Debounce = true;
 
 	-- Update the index
 	History.Index = History.Index + 1;
@@ -73,7 +100,10 @@ function History.Add(Record)
 
 	-- Fire the Changed event
 	History.Changed:Fire();
-
+	
+	-- Release debounce lock
+	History.Debounce = false;
+	
 end;
 
 return History;
