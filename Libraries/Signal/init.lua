8e0b2940301bd9b178023a7e3c864eafa800dad4
Jim Jimminson <bombaltcrismon@gmail.com>
2025-09-19 14:58:59 -0700

    Another signal update...sigh

--- Libraries/Signal/init.lua
+++ Libraries/Signal/init.lua
@@ -97,6 +97,7 @@ function Connection.Disconnect<T...>(self: Connection<T...>)
 			prev = rawget(prev, "_next")
 		end
 		if prev then
+			assert(rawget(prev, "_next") == self, "Bad state")
 			rawset(prev, "_next", ourNext)
 		end
 	end
@@ -167,6 +168,16 @@ function Signal.Connect<T...>(self: Signal<T...>, fn: SignalHandler<T...>): Conn
 	return connection
 end
 
+function Signal:GetConnectionCount()
+	local n = 0
+	local prev = self._handlerListHead
+	while prev do
+		n += 1
+		prev = rawget(prev, "_next")
+	end
+	return n
+end
+
 --[=[
 	Disconnects all connected events to the signal.
 
@@ -176,6 +187,12 @@ end
 	:::
 ]=]
 function Signal.DisconnectAll<T...>(self: Signal<T...>): ()
+	while self._handlerListHead do
+		local last = self._handlerListHead
+		last:Disconnect()
+		assert(self._handlerListHead ~= last, "self._handlerListHead should not be last")
+	end
+
 	self._handlerListHead = false
 end
 
